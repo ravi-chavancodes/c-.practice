e3579ca1d29f43d827e09eea33e515d2d39aeadb
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-07 16:41:24 +0530

    duplicate remover

--- recursion2.cpp
+++ recursion2.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-  // tiling problem
+ /* // tiling problem
 
    int tile(int n ){
 
@@ -16,3 +17,35 @@ int main(){
   
     return 0;
 }
+    */
+
+    // remove duplicate in string
+
+    void removeduplicates( string str , string ans , int i , int map[26]){
+     
+      if(i == str.size()){
+    cout << "ans : " << ans << endl;
+    return;
+      }
+      char ch = str[i];
+      int mapidx = (int)(ch -'a');  // a=0 , b =1 
+  
+
+      if (map [mapidx] == true ) {
+        removeduplicates(str,ans,i+1,map);  // duplicate 
+      }
+     else{
+      map[mapidx] =true;
+      removeduplicates(str,ans+str[i],i+1,map);  // not duplicate
+     }
+    } 
+
+    int main(){
+      string str = "appnacollege";
+      string ans =" ";
+      int map [26]={false};
+
+      removeduplicates (str , ans , 0 ,map);
+
+      return 0;
+    }
