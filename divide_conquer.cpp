54fbc2bc387e02971876a67436e72cfe8be956a5
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-16 16:20:45 +0530

    rotated sort

--- divide_conquer.cpp
+++ divide_conquer.cpp
@@ -138,3 +138,61 @@ return 0;
 }
 */
 
+ /* // rotated sorted 
+
+  #include <iostream>
+using namespace std;
+
+int search(int arr[], int si, int ei, int tar) {
+
+    if (si > ei) {
+        return -1;
+    }
+
+    int mid = si + (ei - si) / 2;
+
+    // Target found
+    if (arr[mid] == tar) {
+        return mid;
+    }
+
+    // Left half is sorted
+    if (arr[si] <= arr[mid]) {
+
+        // Target lies in left half
+        if (arr[si] <= tar && tar <= arr[mid]) {
+            return search(arr, si, mid - 1, tar);
+        }
+        // Search in right half
+        else {
+            return search(arr, mid + 1, ei, tar);
+        }
+    }
+
+    // Right half is sorted
+    else {
+
+        // Target lies in right half
+        if (arr[mid] <= tar && tar <= arr[ei]) {
+            return search(arr, mid + 1, ei, tar);
+        }
+        // Search in left half
+        else {
+            return search(arr, si, mid - 1, tar);
+        }
+    }
+}
+
+int main() {
+
+    int arr[] = {4, 5, 6, 7, 0, 1, 2};
+    int n = 7;
+    int target = 2;
+
+    cout << "Index: " << search(arr, 0, n - 1, target);
+
+    return 0;
+}
+    */
+
+    
