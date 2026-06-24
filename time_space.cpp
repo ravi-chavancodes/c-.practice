d84819f6320edf82d6ce142dbe2786f6b159d9e5
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-24 19:22:18 +0530

    single loop

--- time_space.cpp
+++ time_space.cpp
@@ -1,3 +1,19 @@
 #include <iostream>
 using namespace std;
 
+/* // single loop TC: O(n) , SC: O(1)
+
+int main()
+{
+    int n = 5;
+    for(int i = 0; i < n; i++)  // Loop runs n times. * No extra data structure.
+
+    {
+        cout << i << " ";
+    }
+    return 0;
+}
+ */
+
+
+ 
