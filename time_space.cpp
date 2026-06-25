8df6934a506102c19dd7edc87af4ed15a01724c5
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-25 18:00:10 +0530

    loops

--- time_space.cpp
+++ time_space.cpp
@@ -40,3 +40,27 @@ void printPairs(vector<int>& arr) {
     }
 }
     */
+
+// O(log n) Time , O(1) Space
+
+int binarySearch(vector<int>& arr, int target) {
+
+    int low = 0;
+    int high = arr.size() - 1;
+
+    while(low <= high) {
+
+        int mid = low + (high - low) / 2;
+
+        if(arr[mid] == target)
+            return mid;
+
+        else if(arr[mid] < target)
+            low = mid + 1;
+
+        else
+            high = mid - 1;
+    }
+
+    return -1;
+}
