bab190b02349664489388abd3c08a10f4da72033
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-27 21:16:03 +0530

    array backtracking

---
+++ back_track.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+#include<vector>
+ using namespace std;
+
+  // backtracking on arr 
+
+  void printarr(int arr[], int n){ // print 0-n
+    for (int i = 0; i < n; i++)
+    {
+        cout<< arr[i]<<" ";
+    }
+    cout<<endl;
+  }
+
+  void changearr(int arr[],int n , int i){
+    if (i==n)
+    {
+        printarr(arr,n);
+        return ;
+    }
+    
+    arr[i]=i+1;
+    changearr(arr,n,i+1);
+    arr[i]-= 2;
+  }
+  
+  int main (){
+    int arr[5]={0};
+    int n = 5;
+
+    changearr(arr,n,0);
+    printarr(arr,n);
+    return 0;
+  }
