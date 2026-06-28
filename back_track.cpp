e56ec1f00f0aa4797140f13ad36ffa4cdbe213cf
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-28 16:21:40 +0530

    permutation

--- back_track.cpp
+++ back_track.cpp
@@ -73,4 +73,27 @@ int main() {
 }
     */
 
-    
+    // permutation
+void permutation(string str , string ans){
+     
+   int n = str.size();
+
+   if(n==0){
+        cout<<ans<<"\n";
+        return;
+   }
+   for (int i = 0; i < str.size(); i++)
+   {
+    char ch = str[i];
+    string nextStr = str.substr(0, i) + str.substr(i+1, n-i-1);
+    permutation(nextStr, ans + ch);
+   }
+   
+}
+    int main(){
+      string str = "abc";
+      string ans =" ";
+      
+      permutation(str,ans);
+      return 0;
+    }
