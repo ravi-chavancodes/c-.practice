eb0b8fcd107cb8b121b32d53dfaeecfe5955a633
ravi-chavancode <ravisingh79471@gmail.com>
2026-06-30 22:21:40 +0530

    push_front

--- linked_list.cpp
+++ linked_list.cpp
@@ -3,44 +3,47 @@ using namespace std;
 
 /* // basic structure of linked list
 class node{
+public:
     int data;
     node* next;
 
-    public:
-         node(int val){
-            data = val;
-            next = NULL;
-         }
+    node(int val){
+        data = val;
+        next = NULL;
+    }
 };
 
 class list{
     node* head;
     node* tail;
 
-    public:
+public:
     list(){
-        head=NULL;
-        tail=NULL;
+        head = NULL;
+        tail = NULL;
     }
-    void pushfront(int val) {
-         Node* newNode = new Node(val); //dynamicz
 
-        if(head==NULL)
+    void pushfront(int val) {
+        node* newNode = new node(val); // dynamic
+
+        if(head == NULL)
         {
-         head=tail=newNode
-         }
-         else{
-              newNode->next = head
-          }
-              }
+            head = tail = newNode;
+        }
+        else{
+            newNode->next = head;
+            head = newNode;
+        }
+    }
 };
+
 int main(){
-    list ll();
-    ll.push_front(3);
-    ll.push_front(2);
-    ll.push_front(1);
+    list ll;
+
+    ll.pushfront(3);
+    ll.pushfront(2);
+    ll.pushfront(1);
+
     return 0;
 }
-    */
-
-    // 
+*/
