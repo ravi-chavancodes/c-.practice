7976fe86a3d65a9c64dcf14da08672a1cae40277
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-09 17:25:52 +0530

    back

--- STL_LL.cpp
+++ STL_LL.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main(){
 
     list<int>ll;
-    
+
+    ll.push_front (2);
+    ll.push_front (1); //. 1-2
+
+    ll.push_back (3); //.  1->2->3
+    ll.push_back(4); //.   1->2->4
+
 }
