86179baf19966aaec4ede2d03a6b120f4a6d9ab5
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-10 18:04:51 +0530

    zigzagll

--- STL_LL.cpp
+++ STL_LL.cpp
@@ -35,3 +35,120 @@ int main(){
     */
 
 // merge sort using ll 
+
+// zig zag using ll 
+
+// Node
+class Node {
+public:
+    int data;
+    Node* next;
+
+    Node(int val) {
+        data = val;
+        next = NULL;
+    }
+};
+
+// Linked List
+class List {
+public:
+    Node* head;
+    Node* tail;
+
+    List() {
+        head = NULL;
+        tail = NULL;
+    }
+
+    // Insert at end
+    void push_back(int val) {
+        Node* newNode = new Node(val);
+
+        if(head == NULL) {
+            head = tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        tail = newNode;
+    }
+
+    // Print Linked List
+    void print() {
+        Node* temp = head;
+
+        while(temp != NULL) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+
+    // Zig-Zag Linked List
+    void zigZag() {
+
+        // Step 1 : Find Middle
+        Node* slow = head;
+        Node* fast = head->next;
+
+        while(fast != NULL && fast->next != NULL) {
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        Node* mid = slow;
+
+        // Step 2 : Reverse Second Half
+        Node* curr = mid->next;
+        mid->next = NULL;
+
+        Node* prev = NULL;
+        Node* next = NULL;
+
+        while(curr != NULL) {
+            next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Step 3 : Merge Alternately
+        Node* left = head;
+        Node* right = prev;
+
+        while(left != NULL && right != NULL) {
+
+            Node* nextL = left->next;
+            Node* nextR = right->next;
+
+            left->next = right;
+            right->next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+};
+
+int main() {
+
+    List ll;
+
+    ll.push_back(1);
+    ll.push_back(2);
+    ll.push_back(3);
+    ll.push_back(4);
+    ll.push_back(5);
+    ll.push_back(6);
+
+    cout << "Original Linked List:" << endl;
+    ll.print();
+
+    ll.zigZag();
+
+    cout << "Zig-Zag Linked List:" << endl;
+    ll.print();
+
+    return 0;
+}
