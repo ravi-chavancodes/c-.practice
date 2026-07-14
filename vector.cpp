ff720dd6e38ea24c57df350cdee1e469acf453ad
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-14 17:02:41 +0530

    home

--- vector.cpp
+++ vector.cpp
@@ -89,3 +89,49 @@ int main(){
     */
 
     
+int main()
+{
+    // Roof
+    for(int i = 0; i < 5; i++)
+    {
+        for(int j = 0; j < 5 - i; j++)
+            cout << " ";
+
+        cout << "/";
+
+        for(int j = 0; j < 2 * i; j++)
+            cout << " ";
+
+        cout << "\\" << endl;
+    }
+
+    // Roof base
+    cout << " /________\\" << endl;
+
+    // House
+    for(int i = 1; i <= 6; i++)
+    {
+        cout << "|";
+
+        for(int j = 1; j <= 8; j++)
+        {
+            // Window
+            if((i == 2 || i == 3) && (j == 6 || j == 7))
+                cout << "[]";
+
+            // Door
+            else if((i >= 3) && (j == 3 || j == 4))
+                cout << "|";
+
+            else
+                cout << " ";
+        }
+
+        cout << "|" << endl;
+    }
+
+    // Bottom
+    cout << "|__________|" << endl;
+
+    return 0;
+}
