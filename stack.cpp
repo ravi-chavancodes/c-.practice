08f056de34b46880b0b89fab91c76660c2cdf4e3
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-15 20:49:00 +0530

    ll

--- stack.cpp
+++ stack.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <list>
 using namespace std;
 
 /*  // stack
@@ -50,3 +52,52 @@ int main() {
 }
 */
 
+/*  // ll in stack
+
+template <class T>
+class Stack {
+    list<T> ll;
+
+public:
+    // Push
+    void push(T val) {
+        ll.push_front(val);
+    }
+
+    // Pop
+    void pop() {
+        if (!isempty()) {
+            ll.pop_front();
+        }
+    }
+
+    // Top
+    T top() {
+        if (isempty()) {
+            cout << "Stack is Empty" << endl;
+            return T();
+        }
+        return ll.front();
+    }
+
+    // Check if Empty
+    bool isempty() {
+        return ll.empty();
+    }
+};
+
+int main() {
+    Stack<int> s;
+
+    s.push(3);
+    s.push(2);
+    s.push(1);
+
+    while (!s.isempty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;s
+}
+    */
