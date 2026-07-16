cfdebb9a0d8d76bbd35ad719f66780543647333e
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-16 16:42:59 +0530

    ll

--- stack.cpp
+++ stack.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <string>
 #include <list>
+#include<stack>
 using namespace std;
 
 /*  // stack
@@ -98,6 +99,43 @@ int main() {
         s.pop();
     }
 
-    return 0;s
+    return 0;
 }
     */
+
+    // push at bottom of stack
+
+void pushAtBottom(stack<int> &s, int x) {
+    // Base case
+    if (s.empty()) {
+        s.push(x);
+        return;
+    }
+
+    // Save top element
+    int top = s.top();
+    s.pop();
+
+    // Push x at bottom
+    pushAtBottom(s, x);
+
+    // Put saved element back
+    s.push(top);
+}
+
+int main() {
+    stack<int> s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    pushAtBottom(s, 4);
+
+    while (!s.empty()) {
+        cout << s.top() << " ";
+        s.pop();
+    }
+
+    return 0;
+}
