2c99508698d4688149fe24db4e6a0f3aca8e4a50
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-20 22:03:42 +0530

    parant

--- stack.cpp
+++ stack.cpp
@@ -289,3 +289,58 @@ int main()
     return 0;
 }
     */
+
+    //valid paranthesis
+
+    #include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(string str) {
+
+    stack<char> s;
+
+    for (int i = 0; i < str.size(); i++) {
+
+        char ch = str[i];
+
+        // Opening brackets
+        if (ch == '(' || ch == '[' || ch == '{') {
+            s.push(ch);
+        }
+
+        // Closing brackets
+        else {
+
+            if (s.empty()) {
+                return false;
+            }
+
+            char top = s.top();
+
+            if ((top == '(' && ch == ')') ||
+                (top == '[' && ch == ']') ||
+                (top == '{' && ch == '}')) {
+
+                s.pop();
+            }
+            else {
+                return false;
+            }
+        }
+    }
+
+    return s.empty();
+}
+
+int main() {
+
+    string str1 = "({[]})";
+    string str2 = "([)]";
+
+    cout << isValid(str1) << endl;
+    cout << isValid(str2) << endl;
+
+    return 0;
+}
