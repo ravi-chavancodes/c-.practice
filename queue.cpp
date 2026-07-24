322a4504e95719c4f9e0ce28ef78889f215cb4f4
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-24 20:29:10 +0530

    stl

--- queue.cpp
+++ queue.cpp
@@ -92,7 +92,7 @@ int main() {
 }
     */
    
- /*   // circular queue implementation
+ /*  // circular queue implementation
 
 class Queue {
     int *arr;
@@ -178,3 +178,23 @@ int main() {
 }
     */
    
+    //stl
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+
+    q.push(1);
+    q.push(2);
+    q.push(3);
+
+    while (!q.empty()) {
+        cout << q.front() << endl;
+        q.pop();
+    }
+
+    return 0;
+}
