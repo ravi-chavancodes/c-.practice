b5583308bb5950adaa62851aff52b975b3fe0413
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-24 22:57:26 +0530

    stl

--- queue.cpp
+++ queue.cpp
@@ -254,4 +254,61 @@ int main() {
     return 0;
 }
     */
-   
+
+    // stack using 2 queue
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+class Stack {
+    queue<int> q1;
+    queue<int> q2;
+
+public:
+    void push(int data) {
+        // Move all elements from q1 to q2
+        while (!q1.empty()) {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        // Push new element into q1
+        q1.push(data);
+
+        // Move all elements back to q1
+        while (!q2.empty()) {
+            q1.push(q2.front());
+            q2.pop();
+        }
+    }
+
+    void pop() {
+        if (!q1.empty()) {
+            q1.pop();
+        }
+    }
+
+    int top() {
+        return q1.front();
+    }
+
+    bool empty() {
+        return q1.empty();
+    }
+};
+
+int main() {
+    Stack s;
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    while (!s.empty()) {
+        cout << s.top() << endl;
+        s.pop();
+    }
+
+    return 0;
+}
