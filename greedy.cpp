b57ac795188463e1b93fc890840646e9083b21c6
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-26 21:27:21 +0530

    max

--- greedy.cpp
+++ greedy.cpp
@@ -1,3 +1,40 @@
 #include<iostream>
 using namespace std;
 
+  // max no of acticity
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int maxActivities(vector<int> start, vector<int> end) {
+
+    // A0 select
+    cout << "selecting A0\n";
+
+    int count = 1;
+    int currEndTime = end[0];
+
+    for (int i = 1; i < start.size(); i++) {
+
+        if (start[i] >= currEndTime) { // non-overlapping
+            cout << "selecting A" << i << endl;
+
+            count++;
+            currEndTime = end[i];
+        }
+    }
+
+    return count;
+}
+
+int main() {
+
+    vector<int> start = {1, 3, 0, 5, 8, 5};
+    vector<int> end = {2, 4, 6, 7, 9, 9};
+
+    cout << "Maximum activities = "
+         << maxActivities(start, end) << endl;
+
+    return 0;
+}
