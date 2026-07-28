bc4320306ea44b09c0d3ec7b6eb94f61532a1f1a
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-28 17:33:58 +0530

    max

--- greedy.cpp
+++ greedy.cpp
@@ -45,13 +45,14 @@ bool compare(pair<int, int> p1, pair<int, int> p2)
 
 int main()
 {
+    vector<int> start = {0, 1, 2};
     vector<int> end = {9, 2, 4};
 
     vector<pair<int, int>> activity(3, make_pair(0, 0));
 
-    activity[0] = make_pair(0, 9);
-    activity[1] = make_pair(1, 2);
-    activity[2] = make_pair(2, 4);
+    activity[0] = make_pair(start[0], end[0]);
+    activity[1] = make_pair(start[1], end[1]);
+    activity[2] = make_pair(start[2], end[2]);
 
     for (int i = 0; i < activity.size(); i++) {
         cout << "A" << i << ": "
@@ -72,4 +73,3 @@ int main()
     return 0;
 }
     */
-   
