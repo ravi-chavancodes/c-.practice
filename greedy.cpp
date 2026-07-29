172e6d0d8a2230b3537537959a9cbcf2b662d155
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-29 18:26:13 +0530

    max

--- greedy.cpp
+++ greedy.cpp
@@ -73,3 +73,57 @@ int main()
     return 0;
 }
     */
+
+    // fractional knapsack
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+bool compare(pair<double, int> p1, pair<double, int> p2) {
+    return p1.first > p2.first;
+}
+
+int fractionalKnapsack(vector<int> val, vector<int> wt, int W) {
+    int n = val.size();
+
+    vector<pair<double, int>> ratio(n, make_pair(0.0, 0));
+    // pair(ratio, idx) → (double, int)
+
+    for(int i = 0; i < n; i++) {
+        double r = val[i] / (double)wt[i];
+        ratio[i] = make_pair(r, i);
+    }
+
+    sort(ratio.begin(), ratio.end(), compare);
+
+    int ans = 0;
+
+    for(int i = 0; i < n; i++) {
+        int idx = ratio[i].second;
+
+        if(wt[idx] <= W) {
+            ans += val[idx];
+            W -= wt[idx];
+        }
+        else {
+            ans += ratio[i].first * W;
+            W = 0;
+            break;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    vector<int> val = {60, 100, 120};
+    vector<int> wt = {10, 20, 30};
+
+    int W = 50;
+
+    cout << fractionalKnapsack(val, wt, W) << endl;
+
+    return 0;
+}
+    
