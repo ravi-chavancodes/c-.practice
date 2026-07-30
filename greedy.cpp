e5f1636e79c6582dd8c1949f99981cd7f191d8c7
ravi-chavancode <ravisingh79471@gmail.com>
2026-07-30 17:45:16 +0530

    coin

--- greedy.cpp
+++ greedy.cpp
@@ -258,4 +258,68 @@ int main() {
 }
     */
 
-    
+    // job sequence variation
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Job {
+public:
+    int idx;
+    int deadline;
+    int profit;
+
+    Job(int idx, int deadline, int profit) {
+        this->idx = idx;
+        this->deadline = deadline;
+        this->profit = profit;
+    }
+};
+
+int maxProfit(vector<pair<int, int>> pairs) {
+    int n = pairs.size();
+
+    vector<Job> jobs;
+
+    for (int i = 0; i < n; i++) {
+        jobs.emplace_back(i, pairs[i].first, pairs[i].second);
+    }
+
+    sort(jobs.begin(), jobs.end(), [](Job &a, Job &b) {
+        return a.profit > b.profit;
+    });
+
+    cout << "Selecting Job " << jobs[0].idx << endl;
+
+    int profit = jobs[0].profit;
+    int safeDeadline = 2;
+
+    for (int i = 1; i < n; i++) {
+        if (jobs[i].deadline >= safeDeadline) {
+            cout << "Selecting Job " << jobs[i].idx << endl;
+            profit += jobs[i].profit;
+            safeDeadline++;
+        }
+    }
+
+    cout << "Max Profit = " << profit << endl;
+
+    return profit;
+}
+
+int main() {
+    int n = 4;
+
+    vector<pair<int, int>> jobs(n, make_pair(0, 0));
+
+    jobs[0] = make_pair(4, 20);
+    jobs[1] = make_pair(1, 10);
+    jobs[2] = make_pair(1, 40);
+    jobs[3] = make_pair(1, 30);
+
+    maxProfit(jobs);
+
+    return 0;
+}
