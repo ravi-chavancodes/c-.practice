c74375ba5869638fe6dab48530ed0d550f2898bd
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-01 17:17:41 +0530

    coin

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -60,4 +60,109 @@ int main() {
     return 0; // Program ends successfully
 }
     */
-   
+
+    // inorder , postorder , preoder
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data) {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+int idx = -1;
+
+Node* buildTree(vector<int> &nodes) {
+
+    idx++;
+
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* currNode = new Node(nodes[idx]);
+
+    currNode->left = buildTree(nodes);
+    currNode->right = buildTree(nodes);
+
+    return currNode;
+}
+
+
+// Preorder: Root -> Left -> Right
+void preorder(Node* root) {
+
+    if (root == NULL) {
+        return;
+    }
+
+    cout << root->data << " ";
+
+    preorder(root->left);
+
+    preorder(root->right);
+}
+
+
+// Inorder: Left -> Root -> Right
+void inorder(Node* root) {
+
+    if (root == NULL) {
+        return;
+    }
+
+    inorder(root->left);
+
+    cout << root->data << " ";
+
+    inorder(root->right);
+}
+
+
+// Postorder: Left -> Right -> Root
+void postorder(Node* root) {
+
+    if (root == NULL) {
+        return;
+    }
+
+    postorder(root->left);
+
+    postorder(root->right);
+
+    cout << root->data << " ";
+}
+
+
+int main() {
+
+    vector<int> nodes = {
+        1, 2, 4, -1, -1, 5, -1, -1,
+        3, -1, 6, -1, -1
+    };
+
+    Node* root = buildTree(nodes);
+
+    cout << "Preorder: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Inorder: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Postorder: ";
+    postorder(root);
+    cout << endl;
+
+    return 0;
+}
