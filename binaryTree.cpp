5aeebbda77188e6ec5a23204471105a60c8cdd42
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-03 16:49:36 +0530

    coin

--- binaryTree.cpp
+++ binaryTree.cpp
@@ -436,3 +436,61 @@ int main() {
 }
     */
 
+    // count of nodes 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+int idx = -1;
+
+Node* buildTree(vector<int> nodes) {
+    idx++;
+
+    if (nodes[idx] == -1) {
+        return NULL;
+    }
+
+    Node* newNode = new Node(nodes[idx]);
+
+    newNode->left = buildTree(nodes);
+    newNode->right = buildTree(nodes);
+
+    return newNode;
+}
+
+int count(Node* root) {
+
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftCount = count(root->left);
+    int rightCount = count(root->right);
+
+    return leftCount + rightCount + 1;
+}
+
+int main() {
+
+    vector<int> nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+
+    Node* root = buildTree(nodes);
+
+    cout << "Count : " << count(root) << endl;
+
+    return 0;
+}
