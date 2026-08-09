b58821af827555562a0ff07633ac5bce85f820fd
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-09 15:33:27 +0530

    del

--- vector.cpp
+++ vector.cpp
@@ -30,3 +30,61 @@ cout << endl;
   }
 }
   */
+
+ /* //pair sum
+
+vector<int> pairsum (vector<int> arr , int target )
+{
+int st = 0 , end = arr.size() -1 ;
+int currsum = 0;
+vector<int> ans ;
+
+while(st < end) {
+      currsum = arr[st] + arr[end];
+        if (currsum == target) {
+            ans.push_back(st); 
+            ans.push_back(end) ;
+            return ans;
+        }
+        else if(currsum > target) {
+         end -- ;
+     }
+        else {
+         st++ ;
+ 
+       }
+    }
+    return ans ;
+}
+int main()
+{
+vector<int> vec = {2,7,11,15};
+int target = 9;
+vector<int> ans = pairsum (vec,target);
+cout<< ans[0] <<"," << ans[1]<<endl;
+
+
+return 0;
+}
+*/
+
+  /*  // 2d vector   
+int main(){
+     vector < vector<int>> matrix = {{1,2,3},
+                                     {4,5,6},
+                                     {7,8,9}};
+
+          for (int i = 0; i < matrix.size(); i++) // row
+          {
+            for (int j = 0; j  < matrix[i].size(); j++) // column
+            {
+                cout<< matrix[i][j]<<" ";
+            
+            }
+            cout<<endl;
+          }
+                                    
+    return 0 ;
+}
+    */
+
