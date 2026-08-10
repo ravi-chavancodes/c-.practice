9317fae8c5d6790d0452b4199eefe45ca47fa1a2
ravi-chavancode <ravisingh79471@gmail.com>
2026-08-10 18:52:38 +0530

    vector

--- vector.cpp
+++ vector.cpp
@@ -68,23 +68,4 @@ return 0;
 }
 */
 
-  /*  // 2d vector   
-int main(){
-     vector < vector<int>> matrix = {{1,2,3},
-                                     {4,5,6},
-                                     {7,8,9}};
-
-          for (int i = 0; i < matrix.size(); i++) // row
-          {
-            for (int j = 0; j  < matrix[i].size(); j++) // column
-            {
-                cout<< matrix[i][j]<<" ";
-            
-            }
-            cout<<endl;
-          }
-                                    
-    return 0 ;
-}
-    */
-
+ 
