d8b4ff90aa61b8a7cf71cc479d16c8f8cbcdd595
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-08-24 18:08:29 +0530

    pac

--- pac.cpp
+++ pac.cpp
@@ -2,11 +2,16 @@
 using namespace std;
 
 int main(){
-int  n = 20;
+int  n ;
+cout<< "enter no :"<<endl;
+cin>> n;
 
+int sum =0;
    for(int i = 1 ; i <=n ; i++){
-cout<<i<<endl;
 
+sum += i;
    }
+
+cout<< "sum = " << sum <<endl;
 return 0;
 }
