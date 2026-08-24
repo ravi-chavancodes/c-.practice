93b830546f96c7a4567b1bd17ef932cb16cbdb92
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-08-24 17:58:20 +0530

    pac

--- pac.cpp
+++ pac.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main(){
+int  n = 20;
 
-   for(int i = 0 ; i <=5 ; i++){
-cout<<"apna collge"<<endl;
+   for(int i = 1 ; i <=n ; i++){
+cout<<i<<endl;
 
    }
-cout<<endl;
 return 0;
 }
