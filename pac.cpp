b3e2b41129f46830b7d3a092f1e874eb20e962d2
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-08-26 18:42:07 +0530

    pac

--- pac.cpp
+++ pac.cpp
@@ -1,15 +1,4 @@
 #include<iostream>
 using namespace std;
 
-int main(){
-int n ;
-bool isprime = true;
-cin>>n;
-
-for (int i = 2; i <= n -1 ; i++)
-{
-   if(n%i == 0){
-      isprime = false;
-      break;
-   }
-}
+int main()
