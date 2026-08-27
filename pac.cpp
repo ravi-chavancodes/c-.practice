25fce5ef220addbdb0e294a902249c99c2685ade
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-08-27 18:31:07 +0530

    cc

--- pac.cpp
+++ pac.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int main(){
+int n = 5;
 
-   for (int i = 1; i <= 4; i++)
+   for (int i = n; i >= 1; i--)
    {
-      for (int j = 1; j <=i; j++)
+      for (int j = 1; j <=i; j--)
       {
          cout<<"*";
       }
