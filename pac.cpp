f05074660f3c907ef849b500a47278dafd8fbb4c
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-09 20:04:42 +0530

    if

--- pac.cpp
+++ pac.cpp
@@ -1,21 +1,3 @@
 #include <iostream>
 using namespace std;
 
-int factorial (int n){
-    int fact = 1;
-    for (int i = 1; i <=n; i++)
-    {
-        fact = fact * i;
-    }
-    cout<<"facatorial = ("<<n<<")="<<fact<<endl;
-    return fact;
-}
-int main(){
-    factorial(0);
-    factorial(1);
-    factorial(2);
-    factorial(3);
-    factorial(4);
-
-    return 0;
-}
