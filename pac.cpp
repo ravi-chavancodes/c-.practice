6455f57581ea7dfa576e07eb085ed6cac8b80dda
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-09 21:24:24 +0530

    if

--- pac.cpp
+++ pac.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int factorial (int n){
-    int fact = 1;
-    for (int i = 1; i <=n; i++)
-    {
-        fact = fact * i;
-    }
-    cout<<"facatorial = ("<<n<<")="<<fact<<endl;
-    return fact;
-}
-int main(){
-    factorial(0);
-    factorial(1);
-    factorial(2);
-    factorial(3);
-    factorial(4);
+bool isprime(int n){
 
+    for(int i =2 ; i <= n-1; i++){
+        if(n%i){
+            return false;
+        }
+    }
+    return true;
+}
+
+int main(){
+ 
+    cout<<isprime(4)<<endl;
     return 0;
 }
