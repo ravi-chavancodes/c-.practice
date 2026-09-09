393f6e30b4002e5048e8983e384aae39dacce03e
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-09 21:34:06 +0530

    if

--- pac.cpp
+++ pac.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 
 bool isprime(int n){
-
-    for(int i =2 ; i <= n-1; i++){
-        if(n%i){
-            return false;
-        }
+if (n == 1)
+{
+    return false;
+}
+for (int i = 2; i <= n-1; i++)
+{
+    if(n%i==0){
+        return false;
     }
-    return true;
+}
+return true;
 }
 
 int main(){
  
-    cout<<isprime(4)<<endl;
+    cout<<isprime(3)<<endl;
     return 0;
 }
