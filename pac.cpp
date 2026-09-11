c73dcf380550f038856369b67e02f1dfdd828095
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-11 21:56:31 +0530

    if

--- pac.cpp
+++ pac.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
 
-bool isprime(int n){
-if (n == 1)
-{
-    return false;
+int factorial(int n){
+int fact = 1;
+for(int i=1 ; i<= n ;i++){
+fact *= i;
 }
-for (int i = 2; i <= n-1; i++)
-{
-    if(n%i==0){
-        return false;
-    }
+return fact;
 }
-return true;
+
+int bincoeffecient(int n , int r){
+    
 }
 
 int main(){
