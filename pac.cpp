d9a69790643795055a1f2cf7eb83957b5165dbef
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-15 19:07:53 +0530

    if

--- pac.cpp
+++ pac.cpp
@@ -1,10 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int 
+int sum(int a , int b){
+    cout<<(a+b)<<endl;
+    return a+b;
+}
+float sum1(float a , float b){
+    cout<<(a+b)<<endl;
+    return a+b;
+}
 
 int main(){
- 
-    cout<<bincoeffecient(5,2)<<endl;
+ sum (2,3);
+ sum1(1.5,2.5);
     return 0;
 }
