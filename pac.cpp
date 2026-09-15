ec757e9d99d0b9d9392af23bd760464eeb75bfd3
ravi-chavancode <ravichavan21.dev@gmail.com>
2026-09-15 19:18:42 +0530

    func

--- pac.cpp
+++ pac.cpp
@@ -1,17 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a , int b){
-    cout<<(a+b)<<endl;
-    return a+b;
+int sum(int a, int b){
+    cout << a + b << endl;
+    return a + b;
 }
-float sum1(float a , float b){
-    cout<<(a+b)<<endl;
-    return a+b;
+
+float sum(float a, float b){
+    cout << a + b << endl;
+    return a + b;
 }
 
 int main(){
- sum (2,3);
- sum1(1.5,2.5);
+    sum(2, 3);
+    sum(1.5f, 2.5f);
+
     return 0;
 }
